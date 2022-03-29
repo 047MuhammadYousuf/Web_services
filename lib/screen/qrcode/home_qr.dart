6d2493f7524720f96d55c861014f621d7ffac42a
Muhammad Yousuf <MuhammadYousuf786678@gmail.com>
2022-03-29 16:34:09 +0500

    Home_screen_add

--- lib/screen/qrcode/home_qr.dart
+++ lib/screen/qrcode/home_qr.dart
@@ -1,10 +1,6 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:web_services/screen/qrcode/qr_create.dart';
 import 'package:web_services/screen/qrcode/qr_scan.dart';
-
-import 'widgets.dart';
 
 class QRHomeScreen extends StatefulWidget {
   const QRHomeScreen({Key? key}) : super(key: key);
